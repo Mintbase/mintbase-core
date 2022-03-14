7ebd6182a688eb382743da57f00bd7d42a4a019d
Till <till@mintbase.io>
2022-03-14 09:51:09 +0000

    Improving build and test process

--- test.sh
+++ test.sh
@@ -9,19 +9,27 @@ fail() {
   exit 1
 }
 
+build_wasm() {
+  cargo "$1-wasm" || fail "Compiling $1"
+  # wasm-opt "wasm/$1.wasm" -Oz \
+  #   -o "wasm/$1-opt.wasm" || fail "Minifying $1"
+  # mv "wasm/$1-opt.wasm" "wasm/$1.wasm"
+}
+
 cargo +nightly fmt || fail "Formatting"
 cargo lint || fail "Linting"
 
 cargo check -p mintbase-deps --features store-wasm --message-format short || fail "Checking store"
+build_wasm store # needs to build here so that checking factory won't fail
+
 cargo check -p mintbase-deps --features factory-wasm --message-format short || fail "Checking factory"
 cargo check -p mintbase-deps --features helper-wasm --message-format short || fail "Checking helper"
-cargo check -p mintbase-deps --features market-wasm --message-format short || fail "Checking market"
+cargo check -p simple-market-contract --message-format short || fail "Checking market"
 cargo check -p mintbase-near-indexer || fail "Checking indexer"
 
-cargo store-wasm || fail "Compiling store"
-cargo factory-wasm || fail "Compiling factory"
-cargo helper-wasm || fail "Compiling helper"
-cargo market-wasm || fail "Compiling market"
+build_wasm factory
+build_wasm helper
+build_wasm market
 cargo indexer || fail "Compiling indexer"
 
 # Sandbox node is sometimes running in the background and causing problems
